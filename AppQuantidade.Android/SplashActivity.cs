129de9ab2ec452002f3e4945dca3703a5df02e26
Samir El Hassan <samirelhassann@gmail.com>
2021-11-10 09:47:55 -0300

    asdasd

---
+++ AppQuantidade.Android/SplashActivity.cs
@@ -0,0 +1,22 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace AppQuantidade.Droid
+{
+    [Activity(Label = "GrapesADM", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Disable activity slide-in animation
+            OverridePendingTransition(0, 0);
+
+        }
+    }
+}
