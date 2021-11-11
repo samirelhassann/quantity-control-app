8dad31a167cc0f907f6609f1b35a2de65ca80002
Samir El Hassan <91634008+samirelhassann@users.noreply.github.com>
2021-11-11 15:46:01 -0300

    Ajustando a Slpash Screen do IOS

--- AppQuantidade.Android/SplashActivity.cs
+++ AppQuantidade.Android/SplashActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace AppQuantidade.Droid
 {
-    [Activity(Label = "GrapesADM", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/MainTheme.Splash", Label = "GrapesADM", Icon = "@mipmap/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
